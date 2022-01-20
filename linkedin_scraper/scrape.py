ec8d78442a4ac4d9f418007c4448c98b84556f24
stephen cyron <stephen.cyron@fdmgroup.com>
2022-01-20 07:17:59 -0500

    Added functionality to write to the excel file. It also prints to stdout

--- linkedin_scraper/scrape.py
+++ linkedin_scraper/scrape.py
@@ -17,14 +17,15 @@ def get_contacts():
     get_headers['csrf-token'] = s.cookies.get('JSESSIONID')[1:-1]
     response = s.get('https://www.linkedin.com/voyager/api/relationships/dash/connections', headers=get_headers, params=get_params)
 
-    profiles = []
     connections = json.loads(response.text)
     for connection in connections.get('included'):
         if 'publicIdentifier' in connection:
-            name = connection.get('firstName') + ' ' + connection.get('lastName') + ', '
+            name = connection.get('firstName') + ' ' + connection.get('lastName') 
             response = s.get('https://www.linkedin.com/voyager/api/identity/profiles/' + connection.get('publicIdentifier') + '/profileContactInfo', headers=get_headers)
             contact_info = json.loads(response.text)
-            name += contact_info.get('data').get('emailAddress')
-            print(name)
+            email = contact_info.get('data').get('emailAddress')
+            contacts.append((name, email))
+            line = name + ', ' + email
+            print(line)
 
     return contacts
