907a452a8bb26429c43c619e148bb5545cfdba23
stephen cyron <stephen.cyron@fdmgroup.com>
2022-01-21 06:34:51 -0500

    Changed filepath and creates Excel file if it doesn't exist

--- linkedin_scraper/main.py
+++ linkedin_scraper/main.py
@@ -1,10 +1,15 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 from scrape import get_contacts
 
-data_file = 'linkedin_scraper/contacts.xlsx'
+filepath = 'contacts.xlsx'
 
-wb = load_workbook(data_file, data_only=True)
-ws = wb['Sheet1']
+try:
+    wb = load_workbook(data_file, data_only=True)
+    ws = wb['Sheet']
+except:
+    wb = Workbook()
+    ws = wb.active
+    ws.append(['Name', 'Email'])
 
 # Delete all Rows other than Headers
 while (ws.max_row > 1):
@@ -14,4 +19,4 @@ while (ws.max_row > 1):
 for contact in get_contacts():
     ws.append([contact[0],contact[1]])
 
-wb.save(filename=data_file)
+wb.save(filename=filepath)
